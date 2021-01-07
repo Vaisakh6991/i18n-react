3e8c210e3b08a498304608b61d74c9b7857af61d
Vaisakh <vaisakhpu007@gmail.com>
2021-01-07 22:55:39 +0530

    loading spinner positioned to center

--- src/i18nProvider.js
+++ src/i18nProvider.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import { Box, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'grid',
+    placeItems: 'center',
+    minHeight: '100vh',
+  },
+}));
 
 const I18nProvider = ({ children }) => {
+  const classes = useStyles();
   const { ready } = useTranslation(null, { useSuspense: false });
   if (ready) {
     return children;
   }
-  return <CircularProgress />;
+  return (
+    <Box className={classes.root}>
+      <CircularProgress size={24} />
+    </Box>
+  );
 };
 
 export default I18nProvider;
